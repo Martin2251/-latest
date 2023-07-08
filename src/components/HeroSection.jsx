ca1e03cc50c1efe4850ce5c3ebe92cbe00d2b9f5
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-07-08 11:30:47 +0100

    styled section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 import styled from "styled-components";
+import { Button } from '../styles/Button';
 
 const HeroSection = () => {
   return (
@@ -12,15 +13,70 @@ const HeroSection = () => {
         <p className='hero-para'>Hello i like to talk alot and alot of text</p>
     </div>
     <Button className="btn hireme-btn">
-        <NavLink to="/contact">HIRE ME</NavLink>
+        <NavLink to="/contact">hire me</NavLink>
 
     </Button>
-    <div className="section-hero-image"></div>
+    <div className="section-hero-image">
+        <picture>
+            <img src="./images/hero.svg" alt=""   />
+        </picture>
+    </div>
 </div>
   </Wrapper>
   )
 }
 
-const Wrapper = styled.section``
+const Wrapper = styled.section`
+  padding: 9rem 0;
+
+  .section-hero-data {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .btn {
+    max-width: 16rem;
+  }
+
+  .hero-top-data {
+    text-transform: uppercase;
+    font-weight: 500;
+    font-size: 1.5rem;
+    color: ${({ theme }) => theme.colors.helper};
+  }
+
+  .hero-heading {
+    text-transform: uppercase;
+    font-size: 6.4rem;
+  }
+
+  .hero-para {
+    margin-top: 1.5rem;
+    margin-bottom: 3.4rem;
+    max-width: 60rem;
+  }
+
+  .section-hero-image {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  picture {
+    text-align: center;
+  }
+
+  .hero-img {
+    max-width: 80%;
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .grid {
+      gap: 7.2rem;
+    }
+  }
+`;
+
 
 export default HeroSection
